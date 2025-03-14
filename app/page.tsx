2481f14164273c357c482328ca4c7545a3043e22
Doms <dlmagat08@proton.me>
2025-03-14 20:48:29 +0800

    basic layout

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,21 @@ export default async function Home({ searchParams }: { searchParams: Promise<{ q
     const data = await fetchData("tokyo")
     console.log(data)
     return (
-        <section>
-            <SearchForm query={query} />
-            <h1>{data.name}</h1>
-            <p>{data.main.temp} °C</p>
-            <p>{data.main.feels_like} °C</p>
-            <p>{data.main.humidity} °C</p>
+        <section className="px-5 py-3 flex items-center justify-center h-screen flex-col">
+            <div>
+                <SearchForm query={query} />
+            </div>
+
+            <div className="flex gap-10 mt-10">
+                <div className="text-center">
+                    <h1>{data.name}</h1>
+                    <p>{data.main.temp} °C</p>
+                </div>
+                <div>
+                    <p>Feels Like: {data.main.feels_like} °C</p>
+                    <p>Humidity: {data.main.humidity} °C</p>
+                </div>
+            </div>
         </section>
     )
 }
